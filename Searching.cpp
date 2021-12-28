0af886c06c272bb4265c8efcb16908867016539f
omhardaha <omprakashhardaha369@gmail.com>
2021-12-28 15:16:10 +0530

    change to complete search

--- Searching.cpp
+++ Searching.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+// question sloved of leetcode-
+// https://github.com/kunal-kushwaha/DSA-Bootcamp-Java/blob/main/assignments/06-searching.md
 
 int linearSearch(int a[], int n, int val)
 {
@@ -7,6 +9,15 @@ int linearSearch(int a[], int n, int val)
     if (a[i] == val)
       return i;
 
+  return -1;//if val not found
+};
+int binarySearch(int a[], int l,int h, int val)
+{
+  // int low =l;
+  // int high =h;
+  int mid =l+(h-l)/2;
+
+  
   return -1;//if val not found
 };
 
