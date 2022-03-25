2e23dc5c98b1e4710b9741f1bc4038828e318adf
omhardaha <omprakashhardaha369@gmail.com>
2022-03-26 01:56:45 +0530

    segment tree

--- Tree/FenwickBinaryIndexTree.cpp
+++ Tree/FenwickBinaryIndexTree.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> fenWick(10);
+void update(int i , int)
+{
 
+}
 int main()
 {
     vector<int> arr = {12, 3, 6, 3, 5, 9, 2, 5, 46};
+    int b = 1;
+    // cout << -b;
+    cout << (b & -b) + 1;
     return 0;
 }
