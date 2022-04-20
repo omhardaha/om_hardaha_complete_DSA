9020f311c31396406f2591a5959d1b23a9ab651b
omhardaha <omprakashhardaha369@gmail.com>
2022-04-20 17:57:15 +0530

    priority done

--- CollegeWork/priority.cpp
+++ CollegeWork/priority.cpp
@@ -11,6 +11,7 @@ bool comp(Item a, Item b)
 {
     return (a.priority > b.priority);
 }
+
 int main()
 {
     int TotalWT = 0, n;
@@ -22,21 +23,21 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        cout << "ID - " << i + 1 << " " << endl
-             << "P - ";
+        cout << "ID - " << i + 1 << " " << endl<< "P - ";
         cin >> BT[i].priority;
         cout << "BT - ";
         cin >> BT[i].BT;
         cout << endl;
         BT[i].ID = i + 1;
     }
+
     sort(BT.begin(), BT.end(), comp);
 
     cout << "No  P  BT  WT  TAT" << endl;
+
     for (int i = 0; i < n; i++)
     {
-        if (i > 0)
-            WT[i] = WT[i - 1] + BT[i - 1].BT;
+        if (i > 0)  WT[i] = WT[i - 1] + BT[i - 1].BT;
         TotalWT += WT[i];
         TAT[i] = BT[i].BT + WT[i];
         cout << BT[i].ID << "   " << BT[i].priority << "   " << BT[i].BT << "   " << WT[i] << "   " << TAT[i] << "   " << endl;
